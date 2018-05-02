9089c7189fe2aa8c56ce2c6f838ccc1ca61de32c
jayceexu <xujaycee@gmail.com>
2018-05-02 01:10:13 -0400

    update action hookcer

---
+++ watchinteractor/src/main/java/bach/jianxu/watchinteractor/BackgroundService.java
@@ -0,0 +1,52 @@
+package bach.jianxu.watchinteractor;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+public class BackgroundService extends Service {
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+    private static String TAG = "BackgroundService";
+    // Handler that receives messages from the thread
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            // Normally we would do some work here, like download a file.
+            // For our sample, we just sleep for 5 seconds.
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // Restore interrupt status.
+                Thread.currentThread().interrupt();
+            }
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1);
+        }
+    }
+
+    public BackgroundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "Creating background service");
+
+
+    }
+}
--- watchinteractor/src/main/java/bach/jianxu/watchinteractor/MainActivity.java
+++ watchinteractor/src/main/java/bach/jianxu/watchinteractor/MainActivity.java
@@ -1,13 +1,20 @@
 package bach.jianxu.watchinteractor;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
-
+    private float x1, x2;
+    private float y1, y2;
+    static final int MIN_DISTANCE = 50;
+    public static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +24,47 @@ public class MainActivity extends WearableActivity {
 
         // Enables Always-on
         setAmbientEnabled();
+
+        // use this to start and trigger a service
+        Context context = getApplicationContext();
+        Intent i= new Intent(context, BackgroundService.class);
+        // potentially add data to the intent
+        i.putExtra("KEY1", "Value to be used by the service");
+        context.startService(i);
+    }
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        //Log.i(TAG, "dispatchTouchEvent.....");
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                float deltaX = x2 - x1;
+                float deltaY = y2 - y1;
+
+                if (Math.abs(deltaX) > MIN_DISTANCE) {
+                    //Toast.makeText(this, "left2right swipe", Toast.LENGTH_SHORT).show();
+                    Log.i(TAG, "swipe horizontally......");
+                } else if (Math.abs(deltaY) > MIN_DISTANCE && y1 < y2) {
+                    Log.i(TAG, "swipe down......");
+
+                } else if (Math.abs(deltaY) > MIN_DISTANCE && y2 < y1) {
+                    Log.i(TAG, "swipe up......");
+
+                } else {
+                    // consider as something else - a screen tap for example
+                    Log.i(TAG, "something else......");
+                }
+                break;
+        }
+
+        return super.dispatchTouchEvent(event);
     }
 }
